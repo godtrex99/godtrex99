0456f6ff74fb0ca9546904e2e25eef9be1b261fb
VVIBU Tamfan <114927952+godtrex99@users.noreply.github.com>
2023-08-02 03:23:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,16 @@ const readme = `\
 
 ⏰ Updated on ${new Date().toUTCString()}
 
+<h2 align="center">👨‍💻 Repositories 👨‍💻</h2>
+<br>
+<div width="100%" align="center">
+  <a align="left" href="https://github.com/godtrex99/clashconfig" title="Clash Config"><img align="left" height="115" src="https://github-readme-stats.vercel.app/api/pin/?username=godtrex99&repo=ClashConfig&theme=react&border_color=61dafb&border_radius=10"></a><a
+<br>
+<br>
+<h4 align="center">
+  <a href="https://github.com/godtrex99?tab=repositories" title="Show More Repositories">🔎 Show More 🔍</a>
+</h4>
+
 ![Progress Bar CI](https://github.com/godtrex99/godtrex99/workflows/Progress%20Bar%20CI/badge.svg)\
 `
 
